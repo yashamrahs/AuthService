ae37dd6543d64aa7977dce0d7c8ff9b14170c1c1
Yash Sharma <yash.amrahs@gmail.com>
2025-06-11 23:53:11 +0530

    Add requests

--- app/src/main/java/com/authService/requests/AuthRequestDTO.java
+++ app/src/main/java/com/authService/requests/AuthRequestDTO.java
@@ -1,4 +1,16 @@
 package com.authService.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthRequestDTO {
+
+    private String username;
+    private String password;
 }
--- app/src/main/java/com/authService/requests/RefreshTokenRequestDTO.java
+++ app/src/main/java/com/authService/requests/RefreshTokenRequestDTO.java
@@ -1,4 +1,12 @@
 package com.authService.requests;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RefreshTokenRequestDTO {
+    private String token;
 }
