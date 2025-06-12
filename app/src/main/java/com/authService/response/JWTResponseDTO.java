8bdc0249ac3065351717ec7c9c41f9059e1dd794
Yash Sharma <yash.amrahs@gmail.com>
2025-06-12 22:42:57 +0530

    Add responses

--- app/src/main/java/com/authService/response/JWTResponseDTO.java
+++ app/src/main/java/com/authService/response/JWTResponseDTO.java
@@ -1,4 +1,17 @@
 package com.authService.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class JWTResponseDTO {
+
+    private String accessToken;
+    private String token;
+    private String userId;
 }
