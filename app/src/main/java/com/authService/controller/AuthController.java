6781466c5862eeab732e6ad2e32f26417a01a87f
Yash Sharma <yash.amrahs@gmail.com>
2025-06-16 23:37:02 +0530

    Add Controller

--- app/src/main/java/com/authService/controller/AuthController.java
+++ app/src/main/java/com/authService/controller/AuthController.java
@@ -1,4 +1,67 @@
 package com.authService.controller;
 
+import com.authService.entities.RefreshToken;
+import com.authService.models.UserInfoDto;
+import com.authService.response.JWTResponseDTO;
+import com.authService.services.JWTService;
+import com.authService.services.RefreshTokenService;
+import com.authService.services.UserDetailsServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@AllArgsConstructor
+@RestController
 public class AuthController {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @PostMapping("auth/v1/signup")
+    public ResponseEntity SignUp(@RequestBody UserInfoDto userInfoDto){
+        try {
+            String userId = userDetailsService.signupUser(userInfoDto);
+            if(Objects.isNull(userId)){
+                return new ResponseEntity<>("Already Exist", HttpStatus.BAD_REQUEST);
+            }
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(userInfoDto.getUsername());
+            String jwtToken = jwtService.GenerateToken(userInfoDto.getUsername());
+            return new ResponseEntity<>(JWTResponseDTO.builder().accessToken(jwtToken).
+                    token(refreshToken.getToken()).userId(userId).build(), HttpStatus.OK);
+        } catch (Exception ex){
+            return new ResponseEntity<>("Exception in User Service", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/auth/v1/ping")
+    public ResponseEntity<String> ping() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            String userId = userDetailsService.getUserByUsername(authentication.getName());
+            if(Objects.nonNull(userId)){
+                return ResponseEntity.ok(userId);
+            }
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<Boolean> checkHealth(){
+        return new ResponseEntity<>(true, HttpStatus.OK);
+    }
 }
--- app/src/main/java/com/authService/controller/TokenController.java
+++ app/src/main/java/com/authService/controller/TokenController.java
@@ -1,4 +1,65 @@
 package com.authService.controller;
 
+import com.authService.entities.RefreshToken;
+import com.authService.requests.AuthRequestDTO;
+import com.authService.requests.RefreshTokenRequestDTO;
+import com.authService.response.JWTResponseDTO;
+import com.authService.services.JWTService;
+import com.authService.services.RefreshTokenService;
+import com.authService.services.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Objects;
+
 public class TokenController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JWTService jwtService;
+
+    @PostMapping("auth/v1/login")
+    public ResponseEntity AuthenticateAndGetToken(@RequestBody AuthRequestDTO authRequestDTO){
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequestDTO.getUsername(), authRequestDTO.getPassword()));
+        if(authentication.isAuthenticated()){
+            RefreshToken refreshToken = refreshTokenService.createRefreshToken(authRequestDTO.getUsername());
+            String userId = userDetailsService.getUserByUsername(authRequestDTO.getUsername());
+
+            if(Objects.nonNull(userId) && Objects.nonNull(refreshToken)){
+                return new ResponseEntity<>(JWTResponseDTO.builder()
+                        .accessToken(jwtService.GenerateToken(authRequestDTO.getUsername()))
+                        .token(refreshToken.getToken())
+                        .build(), HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>("Exception in User Service", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @PostMapping("auth/v1/refreshToken")
+    public JWTResponseDTO refreshToken(@RequestBody RefreshTokenRequestDTO refreshTokenRequestDTO){
+        return refreshTokenService.findByToken(refreshTokenRequestDTO.getToken())
+                .map(refreshTokenService::verifyExpiration)
+                .map(RefreshToken::getUserInfo)
+                .map(userInfo -> {
+                    String accessToken = jwtService.GenerateToken(userInfo.getUsername());
+                    return JWTResponseDTO.builder()
+                            .accessToken(accessToken)
+                            .token(refreshTokenRequestDTO.getToken()).build();
+                }).orElseThrow(() ->new RuntimeException("Refresh Token is not in DB..!!"));
+    }
 }
