ee8d76ecd6f98d104e3f2cd79d2e12e1274301ff
Yash Sharma <yash.amrahs@gmail.com>
2025-06-23 23:57:20 +0530

    Add Kafka Producer

---
+++ app/src/main/java/com/authService/eventProducer/UserInfoProducer.java
@@ -0,0 +1,30 @@
+package com.authService.eventProducer;
+
+import com.authService.models.UserInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserInfoProducer {
+
+    private final KafkaTemplate<String, UserInfoDto> kafkaTemplate;
+
+    @Value("${spring.kafka.topic-json.name}")
+    private String topicJsonName;
+
+    @Autowired
+    UserInfoProducer(KafkaTemplate<String, UserInfoDto> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendEventToKafka(UserInfoDto eventData) {
+        Message<UserInfoDto> message = MessageBuilder.withPayload(eventData)
+                .setHeader(KafkaHeaders.TOPIC, topicJsonName).build();
+        kafkaTemplate.send(message);
+    }
+}
