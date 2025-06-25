5ab9bcfcce7708248f65381048af8ce108d0eabe
Yash Sharma <yash.amrahs@gmail.com>
2025-06-25 21:43:23 +0530

    Add Kafka Serializer

---
+++ app/src/main/java/com/authService/serializer/UserInfoSerializer.java
@@ -0,0 +1,38 @@
+package com.authService.serializer;
+
+import com.authService.models.UserInfoDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class UserInfoSerializer implements Serializer<UserInfoDto> {
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+        Serializer.super.configure(configs, isKey);
+    }
+
+    @Override
+    public byte[] serialize(String s, UserInfoDto userInfoDto) {
+        byte[] val = null;
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            val = objectMapper.writeValueAsString(s).getBytes();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return val;
+    }
+
+    @Override
+    public byte[] serialize(String topic, Headers headers, UserInfoDto data) {
+        return Serializer.super.serialize(topic, headers, data);
+    }
+
+    @Override
+    public void close() {
+        Serializer.super.close();
+    }
+}
